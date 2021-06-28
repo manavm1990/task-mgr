d17c5ee28ea4339da83c2a647b4bca2b780bb4b0
Manav Misra <manavm@visionwebsoft.com>
2021-06-28 18:25:54 -0500

    :speech_balloon: Display title in header component
    Uses service.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  constructor(private titleService: Title) {}
+
+  title: String = '';
+
+  getTitle() {
+    this.title = this.titleService.getTitle();
+  }
+
+  ngOnInit(): void {
+    this.getTitle();
+  }
+}
