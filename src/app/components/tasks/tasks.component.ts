38d7f703cd5ac5d0ec25057f0a096a5c3e72e19a
Manav Misra <manavm@visionwebsoft.com>
2021-06-29 07:04:09 -0500

    :construction: List initial tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+import Task from 'src/app/Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-
-  constructor() { }
+  tasks: Task[] = [];
 
   ngOnInit(): void {
+    this.initTasks();
   }
 
+  initTasks(): void {
+    this.tasks = TASKS;
+  }
 }
