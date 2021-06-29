11a9364aaeb5404c4b689605cbee5396f21139bd
Manav Misra <manavm@visionwebsoft.com>
2021-06-29 07:50:26 -0500

    Add faTimes icon

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import Task from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task',
@@ -8,4 +9,6 @@ import Task from 'src/app/Task';
 })
 export class TaskComponent {
   @Input() task!: Task;
+
+  faTimes = faTimes;
 }
