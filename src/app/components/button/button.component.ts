3aa94f0aef0fa42eea71a2ff8a300495eddf3aeb
Manav Misra <manavm@visionwebsoft.com>
2021-06-30 09:13:14 -0500

    :fire: Remove unnecessary text parameter from button click

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -9,9 +9,9 @@ export class ButtonComponent {
   // ⚠️ Either give default text or mark as optional
   @Input() color?: string;
   @Input() text: string = 'Click';
-  @Output() btnClick = new EventEmitter();
+  @Output() clickedBtn = new EventEmitter<boolean>();
 
-  onClick(txt: string) {
-    this.btnClick.emit();
+  onClick() {
+    this.clickedBtn.emit();
   }
 }
